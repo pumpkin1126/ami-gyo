64f65f06cfe9bb94218e60cb0f35df499cc8ecbd
pumpkin1126 <kenshi1126469@icloud.com>
2018-11-19 16:02:01 +0900

    GameManagerにタイマーの機能とスコア保持の機能を追加し、外部から魚の情報を受け取ってスコアに還元する関数を追加した

---
+++ Ami-gyo2/Assets/Scripts/ScriptableObject/GameParams.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( menuName = "Scriptable/GameParams", fileName = "GameParams" )]
+public class GameParams : ScriptableObject{
+
+	public int TimeLimit{ get{return timeLimit;}}
+
+	[SerializeField]
+	int timeLimit;
+}
