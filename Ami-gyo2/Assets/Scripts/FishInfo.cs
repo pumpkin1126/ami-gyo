b6226b5013f51914817823986f19f4cfbab754ef
pumpkin1126 <kenshi1126469@icloud.com>
2018-11-19 15:23:09 +0900

    前のプロジェクトで書いていたスクリプトを追加

---
+++ Ami-gyo2/Assets/Scripts/FishInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fish{
+	public class FishInfo : MonoBehaviour {
+
+		int weight;
+		int bonusTime;
+		EventType eventName;
+
+		public int Weight{ get{return weight;} }
+		public int BonusTime{ get{return bonusTime;} }
+		public EventType EventName{ get{return eventName;} }
+
+		public FishInfo(int _weight, int _bonusTime, EventType _eventName){
+			weight = _weight;
+			bonusTime = _bonusTime;
+			eventName = _eventName;
+		}
+	}
+}
---
+++ Ami-gyo2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance = null;
+
+	public int RemainingTime{ get{return remainingTime;}}
+	public int Score{ get{return score;}}
+
+	int remainingTime;
+	int score;
+
+	void Awake(){
+		if(GameManager.Instance == null)
+			Instance = this;
+	}
+
+	void Start () {
+		score = 0;
+	}
+	
+	void Update () {
+		
+	}
+
+	public void CalculateScore(FishInfo info){
+		score += info.Weight;
+		time += info.BonusTime;
+
+		Debug.Log("FishInfo\t weight: "+info.Weight+"  time: "+info.BonusTime+"  event: "+info.EventName);
+	}
+}
