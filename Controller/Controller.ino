a77ad9265fc19ce2ac85cf6095ef37f2c0617c0f
aridai <aridai.net@outlook.jp>
2018-11-27 14:29:08 +0900

    プロジェクトを移動した。

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,4 +1,7 @@
+//  可変抵抗の入力電圧ピン
 const int REGISTER_PIN = A0;
+
+//  タクトスイッチの入力電圧ピン
 const int SWITCH_PIN = 7;
 
 void setup()
@@ -15,14 +18,7 @@ void loop()
     bool switchValue = digitalRead(SWITCH_PIN);
     int registerValue = analogRead(REGISTER_PIN);
 
+    //  コンマ区切りの文字列でシリアル通信で渡す。
     sprintf(str, "%d,%d", switchValue, registerValue);
-
     Serial.println(str);
-
-    /*
-    Serial.print("switch:");
-    Serial.print(switchValue ? "1" : "0");
-    Serial.print(", register:");
-    Serial.println(registerValue);
-    */
 }
