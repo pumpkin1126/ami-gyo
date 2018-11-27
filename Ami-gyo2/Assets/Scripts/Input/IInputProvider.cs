4f5f956abf17f1c09659143ed814d943b91bd689
aridai <aridai.net@outlook.jp>
2018-11-27 15:01:54 +0900

    IInputProviderインターフェースを追加した。

---
+++ Ami-gyo2/Assets/Scripts/Input/IInputProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Amigyo.Input
+{
+    /// <summary>
+    /// 入力ソースを提供します。
+    /// 開始時と終了時にはそれぞれ<see cref="Listen"/>と<see cref="Dispose"/>を必ず呼び出してください。
+    /// </summary>
+    public interface IInputProvider : IDisposable
+    {
+        /// <summary>
+        /// アミの発射角度を表す角度を通知・保持します。
+        /// 角度はX軸正向きを始線とする極座標の角度で、度数法表記の値です。
+        /// </summary>
+        ReactiveProperty<float> Angle { get; }
+
+        /// <summary>
+        /// ボタンが押されているかどうかを通知・保持します。
+        /// </summary>
+        ReactiveProperty<bool> IsPressed { get; }
+
+        /// <summary>
+        /// 変更の通知を開始します。
+        /// </summary>
+        /// <returns>成功したかどうかを表すbool値。</returns>
+        bool Listen();
+    }
+}
