d6637d7ba6b35890fe360a5c81e5e594b5e577e3
ryuya5 <i151307@gm.ishikawa-nct.ac.jp>
2018-11-29 16:16:24 +0900

    UI実装完了

---
+++ Ami-gyo2/Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public float countTime = 300;
+	public GameObject Time_object = null;
+
+	void Start () {
+	}
+
+	void Update () {
+	Text Time_text = Time_object.GetComponent<Text>();
+	Time_text.text = "Time:" + countTime;
+	countTime -= Time.deltaTime;
+	}
+}
