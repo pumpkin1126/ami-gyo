7fab28769694b70846392a68d1a105bd7b14783b
pumpkin1126 <kenshi1126469@icloud.com>
2019-01-15 19:51:28 +0900

    群れとなって移動するスクリプトを追加

--- Ami-gyo2/Assets/Scripts/Fishes/Group.cs
+++ Ami-gyo2/Assets/Scripts/Fishes/Group.cs
@@ -6,9 +6,13 @@ namespace Amigyo{
 	namespace Fishes{
 		[RequireComponent (typeof(Migration))]
 		public class Group : MonoBehaviour, IFishBehavior {
-
+			
+			public float ViewRange_r = 7f;
+			[Range(0.0f, 1.0f)]
+			public float View2CrowdedRange = 0.5f;
 			public int FishAmountInGroup;
 			List<Group> otherFishScripts;
+
 
 			public void SetUp(List<Group> fishScripts){
 				otherFishScripts = new List<Group>(fishScripts);
@@ -16,7 +20,38 @@ namespace Amigyo{
 			}
 
 			public Vector3 GetVelocity(Vector3 currentVelocity){
-				return Vector3.zero;
+				Vector3 addV = Vector3.zero;      //周囲の魚の進行方向の総和
+    			Vector3 addLoc = Vector3.zero;    //周囲の魚の重心
+    			Vector3 crowdedAddV = Vector3.zero;    //混雑していない方向の総和
+
+    			float divNumForAddLoc = 0;		//重心を求めるのに用いた魚の数（見えている魚の数だけ位置を足して、この数で割る）
+
+				//上記3つのベクトルを求める
+    			foreach(var fish in otherFishScripts){
+
+    				Vector3 distV = (transform.position - fish.transform.position);
+    				float dist = distV.magnitude;
+    				if(dist > ViewRange_r)  continue;		//視認範囲外の魚は考慮しない
+
+    				addV += fish.GetComponent<Rigidbody>().velocity.normalized;
+    				addLoc += fish.transform.position;
+					divNumForAddLoc++;
+
+					//混雑していると感じる範囲よりも距離が大きければ、この先の計算は必要ない
+    				if(dist > ViewRange_r*View2CrowdedRange)  continue;
+
+    				crowdedAddV += distV.normalized;
+    			}
+
+				//自分以外の魚の重心に向かう方向
+    			Vector3 toCentroid = Vector3.zero;
+    			if(divNumForAddLoc > 0){
+    			  addLoc /= divNumForAddLoc;
+    			  toCentroid = (addLoc - transform.position).normalized;
+    			}
+
+				//周囲の魚の進行方向、重心、混雑していない方向をすべて足したもの
+    			return (addV + toCentroid + crowdedAddV).normalized;
 			}
 
 			public void Die(System.Action<int> decreaseFishCount, int id){
