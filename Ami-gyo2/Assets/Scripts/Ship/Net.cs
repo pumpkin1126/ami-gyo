744340c06aad5c948cc7567de8baec3154392170
pumpkin1126 <kenshi1126469@icloud.com>
2019-01-18 12:38:10 +0900

    netがshooterの正面から放出されない問題を修正し、netの速度をnetのプレハブから変えられるようにした

--- Ami-gyo2/Assets/Scripts/Ship/Net.cs
+++ Ami-gyo2/Assets/Scripts/Ship/Net.cs
@@ -7,13 +7,14 @@ using Amigyo.Fishes;
 public class Net : MonoBehaviour {
 
 	bool isFirst = true; //1度だけ加速したいのでbool型の変数を使いたい
+	public float Speed = 5;
 
 	//shooterによって生成された後、1度だけ加速する
 	void FixedUpdate(){
 		if(isFirst == true){
 			isFirst = false;
 			Rigidbody rb = this.GetComponent<Rigidbody>();
-			rb.AddForce(transform.forward * 5, ForceMode.Impulse);
+			rb.AddForce(transform.forward * Speed, ForceMode.Impulse);
 		}
 	}
 
