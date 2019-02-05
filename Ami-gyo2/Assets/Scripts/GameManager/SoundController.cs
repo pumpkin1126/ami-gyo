c2ab705b075f8057ad0bbc335f295690e54fd195
pumpkin1126 <kenshi1126469@icloud.com>
2019-02-05 15:58:22 +0900

    soundController実装

---
+++ Ami-gyo2/Assets/Scripts/GameManager/SoundController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour {
+
+	AudioSource source;
+
+	void Start(){
+		source = GetComponent<AudioSource>();
+	}
+
+	public void Play(AudioClip clip, bool isBGM){
+		source.clip = clip;
+		source.Play();
+	}
+}
